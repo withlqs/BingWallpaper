e9a0c1da92bba7a714d314c92fb46eb7096af44c
withlqs <withlqs@gmail.com>
2015-01-09 20:08:17 +0800

    core function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import sys
+from urllib.request import urlopen
+import json
 __author__ = 'lqs'
+
+webpage = urlopen('http://www.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1')
+js = webpage.read().decode("utf-8")
+location = json.loads(js)['images'][0]['url']
+
+print(location)
