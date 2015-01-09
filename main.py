862837631e94ddea1ea9971a0db7910002371c4c
withlqs <withlqs@gmail.com>
2015-01-09 20:42:56 +0800

    edit variables

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ set desktop picture to POSIX file "%s"
 end tell
 END"""
 
+url_base = 'http://cn.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1'
+codec = 'utf-8'
+
 def set_wallpaper(file):
     subprocess.Popen(SCRIPT%file, shell=True)
 
-webpage = urllib.request.urlopen('http://cn.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1')
-js = webpage.read().decode("utf-8")
+webpage = urllib.request.urlopen(url_base)
+js = webpage.read().decode(codec)
 download_url = json.loads(js)['images'][0]['url']
 
 local_path = '/Users/' + getpass.getuser() + '/Pictures/bingwallpaper.jpg'
