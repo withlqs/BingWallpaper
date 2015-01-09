3f20c229e628360353db87e0fef27225e673a55e
withlqs <withlqs@gmail.com>
2015-01-09 20:37:31 +0800

    main function finished

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+
 import sys
-from urllib.request import urlopen
+import urllib.request
 import json
+import subprocess
+import getpass
 __author__ = 'lqs'
 
-webpage = urlopen('http://www.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1')
-js = webpage.read().decode("utf-8")
-location = json.loads(js)['images'][0]['url']
+SCRIPT = """/usr/bin/osascript<<END
+tell application "Finder"
+set desktop picture to POSIX file "%s"
+end tell
+END"""
 
-print(location)
+def set_wallpaper(file):
+    subprocess.Popen(SCRIPT%file, shell=True)
+
+webpage = urllib.request.urlopen('http://cn.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1')
+js = webpage.read().decode("utf-8")
+download_url = json.loads(js)['images'][0]['url']
+
+local_path = '/Users/' + getpass.getuser() + '/Pictures/bingwallpaper.jpg'
+urllib.request.urlretrieve(download_url, local_path)
+
+set_wallpaper(local_path)
