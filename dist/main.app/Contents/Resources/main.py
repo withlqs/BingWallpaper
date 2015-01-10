132e3b2c0d085a3ac883b9c982f895991457e53c
withlqs <withlqs@gmail.com>
2015-01-10 19:01:00 +0800

    create .app

---
+++ dist/main.app/Contents/Resources/main.py
@@ -0,0 +1,35 @@
+import sys
+import urllib.request
+import json
+import subprocess
+import getpass
+import time
+
+__author__ = 'lqs'
+__version__ = 0.1
+__page__ = 'https://github.com/withlqs/BingWallpaperForMac'
+
+SCRIPT = """/usr/bin/osascript<<END
+tell application "Finder"
+set desktop picture to POSIX file "%s"
+end tell
+END"""
+
+today = time.strftime("%Y%m%d")
+local_file = 'bingwallpaper'+today+'.jpg'
+local_path = '/Users/' + getpass.getuser() + '/Pictures/'
+file = local_path+local_file
+url_base = 'http://cn.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1'
+codec = 'utf-8'
+print(local_path+local_file)
+
+def set_wallpaper(file):
+    subprocess.Popen(SCRIPT%file, shell=True)
+
+webpage = urllib.request.urlopen(url_base)
+js = webpage.read().decode(codec)
+download_url = json.loads(js)['images'][0]['url']
+print(download_url)
+urllib.request.urlretrieve(download_url, file)
+
+set_wallpaper(file)
