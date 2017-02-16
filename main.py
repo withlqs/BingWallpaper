dbdd0cccb46c1be16027c6f1791e887a0c512085
Qiushi Li <withlqs@gmail.com>
2017-02-16 15:18:34 +0800

    fix error & edit python file format

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import urllib.request
 import json
@@ -7,28 +9,32 @@ import time
 import os
 
 __author__ = 'lqs'
-__version__ = 0.2
+__version__ = 0.3
 __page__ = 'https://github.com/withlqs/BingWallpaper'
 
-SCRIPT = """gsettings set org.gnome.desktop.background picture-uri file://%s"""
-
-today = time.strftime("%Y%m%d")
-local_file = 'bingwallpaper'+today+'.jpg'
-local_path = '/home/' + getpass.getuser() + '/Pictures/'
-file = local_path+local_file
-if os.path.isfile(file):
-    exit()
-url_base = 'http://cn.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1'
-codec = 'utf-8'
-print(local_path+local_file)
-
 def set_wallpaper(file):
+    SCRIPT = """gsettings set org.gnome.desktop.background picture-uri file://%s"""
     subprocess.Popen(SCRIPT%file, shell=True)
 
-webpage = urllib.request.urlopen(url_base)
-js = webpage.read().decode(codec)
-download_url = json.loads(js)['images'][0]['url']
-print(download_url)
-urllib.request.urlretrieve(download_url, file)
 
-set_wallpaper(file)
+def main():
+    today = time.strftime("%Y%m%d")
+    local_file = 'bingwallpaper'+today+'.jpg'
+    local_path = '/home/' + getpass.getuser() + '/Pictures/'
+    file = local_path+local_file
+    if os.path.isfile(file):
+        exit()
+    url_base = 'http://cn.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1'
+    codec = 'utf-8'
+    print(local_path+local_file)
+    webpage = urllib.request.urlopen(url_base)
+    js = webpage.read().decode(codec)
+    download_url = 'http://cn.bing.com'+json.loads(js)['images'][0]['url']
+    print(download_url)
+    urllib.request.urlretrieve(download_url, file)
+    set_wallpaper(file)
+
+
+if __name__ == '__main__':
+    main()
+
